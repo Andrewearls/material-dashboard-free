62e6e1f5d7ffd8cd6167c601a03a2d0268aeb5a2
Andrew Earls <andrewearls@rocketmail.com>
2019-08-16 10:01:58 -0700

    test was succesful reverting

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app', ['class' => 'off-canvas-sidebar', 'activePage' => 'home', 'title' => __('Material Dashboard')])
 
 @section('content')
-{{dd(parse_url(getenv("DATABASE_URL")))}}
 <div class="container" style="height: auto;">
   <div class="row justify-content-center">
       <div class="col-lg-7 col-md-8">
